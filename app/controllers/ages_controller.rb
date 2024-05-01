391dbd477235970cce18de53667c2a1834ca48a4
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-05-01 19:04:26 +0000

    fin

--- app/controllers/ages_controller.rb
+++ app/controllers/ages_controller.rb
@@ -1,10 +1,16 @@
 class AgesController < ApplicationController
 
 def youngest
+  young=Director.where.not({ :dob => nil }).order({ :dob => :desc })
+  @match=young.at(0)
+
   render ({ :template => "age_templates/youngest"})
+  
 end
 
 def eldest
+  old=Director.where.not({ :dob => nil }).order({ :dob => :asc })
+  @match=old.at(0)
   render ({ :template => "age_templates/eldest"})
 end
 end
